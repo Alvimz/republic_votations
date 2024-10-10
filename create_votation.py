d743d9322087216bd6386e1e1fe521a651187e9d
gAlvim <ledyzito@hotmail.com>
2024-10-10 15:38:50 -0300

    arrumando votos

--- create_votation.py
+++ create_votation.py
@@ -1,12 +1,12 @@
 from person import Person
 from random import choice,randint,random,uniform
+from typing import List
 
 class CreateVotation:
     
     def __init__(self) -> None:
-        self.persons = list()
+        self.persons:List[Person] = list()
         self.total_white_votes = 0
-        self.no_voted = 0
         
     def create_persons_list(self,persona:Person):
         self.persons.append(persona)
@@ -14,39 +14,30 @@ class CreateVotation:
     def calculate_white_votes(self,qnt):
         possibily = uniform(0.01,0.25)
         self.total_white_votes=(int(round(possibily*qnt)))
+        return self.total_white_votes
 
-        
     def make_votations(self,qnt):
         self.calculate_white_votes(qnt)
-        votes_no_white_votes =self.no_voted= qnt - self.total_white_votes
-        for candidato in self.persons:
-            votes_candidato = randint(0,votes_no_white_votes)
-            candidato.number_votes += votes_candidato
-            votes_no_white_votes-=votes_candidato
-        self.no_voted = votes_no_white_votes
-        return self.total_white_votes,self.no_voted
-    
+        votes_remaining = qnt - self.total_white_votes
+        while votes_remaining>0:
+            candidato = choice(self.persons)
+            candidato.number_votes+=1
+            votes_remaining-=1
+        return self.total_white_votes    
     
     def show_results(self):
         for canditato in self.persons:
             print(f'{canditato.name} - {canditato.partido} - {canditato.number_votes}')
         print(f'Total de votos brancos: {self.total_white_votes}')
-        print(f'Votos anulados/sem origem: {self.no_voted}')
 
         
-            
-            
-            
 if __name__ == '__main__':
     votacao = CreateVotation()
     gabriel = Person('Gabriel','BR',99)
     dilma = Person('Dilma','PT',13)
+    aecio = Person('Aécio','PSDB',19)
     votacao.create_persons_list(gabriel)
     votacao.create_persons_list(dilma)
-    votacao.make_votations(100)
+    votacao.create_persons_list(aecio)
+    votacao.make_votations(10000)
     votacao.show_results()
-            
-            
-            
-        
-    
